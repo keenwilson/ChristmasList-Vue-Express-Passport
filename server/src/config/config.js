74b70c32f2d143f5112c4db376ba44e45f201735
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-11-19 21:30:35 -0600

    Remove config file out of .gitignore

---
+++ server/src/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+      database: process.env.DB_NAME || 'christmaswishlist',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS,
+      options: {
+        dialect: process.env.DIALECT || 'mysql',
+        host: process.env.HOST || 'localhost'
+      }
+    },
+    authentication: {
+      jwtSecret: process.env.JWT_SECRET || 'secret'
+    }
+  }
+  
