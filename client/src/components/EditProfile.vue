043bc4aa7c1576183a9d36be78663d25acb71709
Micah Walker <minimelon19@gmail.com>
2018-11-21 10:05:59 -0600

    Added Editprofile UI

--- client/src/components/EditProfile.vue
+++ client/src/components/EditProfile.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-layout>
-    <v-flex xs4>
+  <v-layout column>
+    <v-flex xs4 >
       <panel title="Edit Profile">
+        <v-text-field
+          label="Name"
+          v-model="name">
+          </v-text-field>
+           <v-text-field
+          label="Username"
+          v-model="username"
+        ></v-text-field>
+           <br>
+           <div class="error" v-html="error" />
+        <v-btn class="cyan" @click="editProfile" dark>Update Profile</v-btn>
       </panel>
     </v-flex>
     <v-flex xs8>
-
     </v-flex>
   </v-layout>
 </template>
@@ -14,6 +24,13 @@
 import Panel from '@/components/Panel'
 
 export default {
+  data () {
+    return {
+      name: '',
+      username: '',
+      error: null
+    }
+  },
   components: {
     Panel
   }
@@ -21,4 +38,7 @@ export default {
 </script>
 
 <style scoped>
+.error {
+  color: red;
+}
 </style>
